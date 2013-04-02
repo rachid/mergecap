1c896bc049c0e77a3e4febc0a00fda3378db2177
Rachid <rachid@qraft.nl>
2013-04-02 12:26:03 +0200

    Fix path to team.

--- app/assets/javascripts/router.js.coffee
+++ app/assets/javascripts/router.js.coffee
@@ -1,6 +1,6 @@
 Mergecap.Router.map ->
   @route 'members'
-  @resource 'teams', path: '/', ->
+  @resource 'teams', path: '/teams', ->
     @resource 'team', { path: ':team_id' }, ->
       @resource 'project', { path: ':project_id' }
   @route 'dashboard', { path: '/dashboard/:team_id' }
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in!"
+    redirect_to root_url(anchor: 'teams'), notice: "Signed in!"
   end
 
   def destroy
